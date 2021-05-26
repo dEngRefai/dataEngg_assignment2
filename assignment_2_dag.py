3016b094102f3271a82e2122a21952e8164db595
dEngRefai <81707589+dEngRefai@users.noreply.github.com>
2021-05-26 20:21:58 +0300

    Update assignment_2_dag.py

--- assignment_2_dag.py
+++ assignment_2_dag.py
@@ -1,19 +1,24 @@
 import datetime as dt
 from datetime import timedelta
 from airflow import DAG
+
 from airflow.operators.bash_operator import BashOperator
+
 from airflow.operators.python_operator import PythonOperator 
 import pandas as pd
 import psycopg2
 import json
 from sqlalchemy import create_engine
 
+## check if needed for all 
 from random import randint  ## check if needed in the end
+
+
 import subprocess
 
 default_args={
     'owner': 'refai',
-    'start_date': dt.datetime(2021, 5, 11),
+    'start_date': dt.datetime(2021, 5, 26),
     'retries': 1,
     'retry_delay': timedelta(minutes=1)
 }
@@ -46,18 +51,70 @@ def _install_tools():
     except:
         subprocess.check_call(['pip' ,'install', 'pandas' ])
         import pandas as pd 
-        
+ 
+     
     try:
-        import pymongo 
+        import matplotlib 
     except:
-        subprocess.check_call(['pip' ,'install', 'pymongo' ])
-        import pymongo 
-        
+        subprocess.check_call(['pip' ,'install', 'matplotlib' ])
+        import matplotlib
 
-        
+    try:
+        import sklearn 
+    except:
+        subprocess.check_call(['pip' ,'install', 'sklearn' ])
+        import sklearn
+    
 
+    
+
+    
+def extractfromGithub():
+    List_of_days=[]
+    for year in range(2020,2022):
+      for month in range(1,13):
+        for day in range(1,32):
+          month=int(month)
+          if day <=9:
+            day=f'0{day}'
+
+          if month <= 9 :
+            month=f'0{month}'
+          List_of_days.append(f'{month}-{day}-{year}')
+    
+    Day='01-01-2021' #could be updated 
+    import pandas as pd 
+    def Get_DF_i(Day):   
+        DF_i=None
+        try: 
+            URL_Day=f'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{Day}.csv'
+            DF_day=pd.read_csv(URL_Day)
+            DF_day['Day']=Day
+            cond=(DF_day.Country_Region=='Jordan')
+            Selec_columns=['Day','Country_Region', 'Last_Update',
+              'Lat', 'Long_', 'Confirmed', 'Deaths', 'Recovered', 'Active',
+              'Combined_Key', 'Incident_Rate', 'Case_Fatality_Ratio']
+            DF_i=DF_day[cond][Selec_columns].reset_index(drop=True) 
+    
+        except:
+            pass
+        return DF_i
+    
+    DF_all=[]
+    for Day in List_of_days:
+        DF_all.append(Get_DF_i(Day))
         
-def queryPostgresql():
+    DF_Jordan=pd.concat(DF_all).reset_index(drop=True)
+    
+    # Create DateTime for Last_Update
+    DF_Jordan['Last_Updat']=pd.to_datetime(DF_Jordan.Last_Update, infer_datetime_format=True)  
+    DF_Jordan['Day']=pd.to_datetime(DF_Jordan.Day, infer_datetime_format=True)  
+
+    DF_Jordan['Case_Fatality_Ratio']=DF_Jordan['Case_Fatality_Ratio'].astype(float)
+        
+    
+    
+    
     # define engine
     host="postgres_storage"
     database="testDB"
@@ -66,45 +123,24 @@ def queryPostgresql():
     port='5432'
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{database}')
     
-    #pull table into dataframe
+    #insert DF_Jordan into postgres
+    
+    DF_Jordan.to_sql('DF_jordan', engine, if_exists='replace', index=False)
+    
+    
+    
+    
+    
+    
+    
+    
     DFpostgress=pd.read_sql("SELECT * FROM users2020" , engine);
     
-    #print to the logs the pulled table
-    print(DFpostgress.head(10))
     
-    #transfor the datafram extracted form postgress to csv file
-    DFpostgress.to_csv('/opt/airflow/dags/frompostgresss.csv') #check mounting of volumes       
+    
+    
         
 
-    #print the name of tables in data base 
-    print(engine.table_names()) # check befor puplish
-        
-
-def CSVToJson():
-    
-    #csv to json
-    df=pd.read_csv('/opt/airflow/dags/frompostgresss.csv') 
-    df.to_json('/opt/airflow/dags/datatojson.json', orient='split', index=False)
-    
-    #print to the logs
-    for i,r in df.iterrows():
-        print(r['name'])
-
-def insertMongoDB():
-    import json
-    from pymongo import MongoClient
-    
-    #connect to MongoDB
-    client = MongoClient('mongo:27017', 
-                        username='root',
-                         password='example')
-    #pushdata
-    db = client['faker_db']
-    collection_fake = db['fakefake']
-
-    with open('/opt/airflow/dags/datatojson.json') as f:
-        file_data = json.load(f)
-        collection_fake.insert_one(file_data)
   
 
 with DAG('postgressCSVJSONmongodbDAG',
@@ -123,4 +159,7 @@ with DAG('postgressCSVJSONmongodbDAG',
     insertData = PythonOperator(task_id='InsertDataMongoDB', 
                              python_callable=insertMongoDB)
 
-install_tools >> getDATA >> CSVJson >>  insertData
+
+install_tools >> extractfromGithub
+
+ 
